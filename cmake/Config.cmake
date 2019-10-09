f11a2f4d27ca119acb1f338491180baddaeeb57b
engineer1109 <1292846099@qq.com>
2019-10-10 00:48:50 +0800

    update code

---
+++ cmake/Config.cmake
@@ -0,0 +1,4 @@
+option(GIT_ThirdParty "git submodule the thirdparty" ON)
+option(USE_OpenMP "Use CPU Multithread OpenMP" ON)
+option(BUILD_BASE "Build libvulkanbase.so" ON)
+option(BUILD_SimpleExamples "Build SimpleExamples" ON)
---
+++ cmake/GCC.cmake
@@ -0,0 +1,8 @@
+if(UNIX)
+    set(CMAKE_EXE_LINKER_FLAGS "-fno-pie -no-pie")
+    add_definitions("-Ofast -std=c++17 -fPIC")
+    add_definitions(-DVK_USE_PLATFORM_XCB_KHR)
+elseif(WIN32)
+    add_definitions(-DNOMINMAX)
+    add_definitions(-DVK_USE_PLATFORM_WIN32_KHR)
+endif()
