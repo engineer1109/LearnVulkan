8b58899109b1743c1735e502ac78ce79afdf47b5
wjl <1292846099@qq.com>
2019-10-10 12:39:20 +0800

    update examples

---
+++ src/basicengine/vulkan_basicengine.cxx
@@ -0,0 +1,149 @@
+#include "vulkan_basicengine.h"
+VulkanBasicEngine::VulkanBasicEngine(bool enableValidation):VulkanExampleBase(enableValidation){
+
+}
+
+VulkanBasicEngine::~VulkanBasicEngine(){
+
+}
+#ifdef __unix__
+void VulkanBasicEngine::initWindow(){
+    this->setupWindow();
+}
+#endif
+
+#ifdef WIN32
+VulkanBasicEngine *g_vulkanExample;                                                                        \
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)                        \
+{                                                                                                    \
+    if (g_vulkanExample != NULL)                                                                        \
+    {                                                                                                \
+        g_vulkanExample->handleMessages(hWnd, uMsg, wParam, lParam);                                    \
+    }                                                                                                \
+    return (DefWindowProc(hWnd, uMsg, wParam, lParam));                                                \
+}
+
+HWND VulkanBasicEngine::initWindow()
+{
+    WNDPROC wndproc=WndProc;
+    HINSTANCE hinstance = ::GetModuleHandle(NULL);
+    g_vulkanExample=this;
+    this->windowInstance = hinstance;
+
+    WNDCLASSEX wndClass;
+
+    wndClass.cbSize = sizeof(WNDCLASSEX);
+    wndClass.style = CS_HREDRAW | CS_VREDRAW;
+    wndClass.lpfnWndProc = wndproc;
+    wndClass.cbClsExtra = 0;
+    wndClass.cbWndExtra = 0;
+    wndClass.hInstance = hinstance;
+    wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wndClass.lpszMenuName = NULL;
+    wndClass.lpszClassName = name.c_str();
+    wndClass.hIconSm = LoadIcon(NULL, IDI_WINLOGO);
+
+    if (!RegisterClassEx(&wndClass))
+    {
+        std::cout << "Could not register window class!\n";
+        fflush(stdout);
+        exit(1);
+    }
+
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+    if (settings.fullscreen)
+    {
+        DEVMODE dmScreenSettings;
+        memset(&dmScreenSettings, 0, sizeof(dmScreenSettings));
+        dmScreenSettings.dmSize = sizeof(dmScreenSettings);
+        dmScreenSettings.dmPelsWidth = screenWidth;
+        dmScreenSettings.dmPelsHeight = screenHeight;
+        dmScreenSettings.dmBitsPerPel = 32;
+        dmScreenSettings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+
+        if ((width != (uint32_t)screenWidth) && (height != (uint32_t)screenHeight))
+        {
+            if (ChangeDisplaySettings(&dmScreenSettings, CDS_FULLSCREEN) != DISP_CHANGE_SUCCESSFUL)
+            {
+                if (MessageBox(NULL, "Fullscreen Mode not supported!\n Switch to window mode?", "Error", MB_YESNO | MB_ICONEXCLAMATION) == IDYES)
+                {
+                    settings.fullscreen = false;
+                }
+                else
+                {
+                    return nullptr;
+                }
+            }
+        }
+
+    }
+
+    DWORD dwExStyle;
+    DWORD dwStyle;
+
+    if (settings.fullscreen)
+    {
+        dwExStyle = WS_EX_APPWINDOW;
+        dwStyle = WS_POPUP | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+    }
+    else
+    {
+        if(enableWindow){
+            dwExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
+            dwStyle = WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+        }
+        else{
+            dwExStyle = WS_EX_APPWINDOW;
+            dwStyle = WS_POPUP | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+        }
+    }
+
+    RECT windowRect;
+    windowRect.left = 0L;
+    windowRect.top = 0L;
+    windowRect.right = settings.fullscreen ? (long)screenWidth : (long)width;
+    windowRect.bottom = settings.fullscreen ? (long)screenHeight : (long)height;
+
+    AdjustWindowRectEx(&windowRect, dwStyle, FALSE, dwExStyle);
+
+    std::string windowTitle = getWindowTitle();
+    window = CreateWindowEx(0,
+        name.c_str(),
+        windowTitle.c_str(),
+        dwStyle | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
+        0,
+        0,
+        windowRect.right - windowRect.left,
+        windowRect.bottom - windowRect.top,
+        NULL,
+        NULL,
+        hinstance,
+        NULL);
+
+    if (!settings.fullscreen)
+    {
+        // Center on screen
+        uint32_t x = (GetSystemMetrics(SM_CXSCREEN) - windowRect.right) / 2;
+        uint32_t y = (GetSystemMetrics(SM_CYSCREEN) - windowRect.bottom) / 2;
+        SetWindowPos(window, 0, x, y, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
+    }
+
+    if (!window)
+    {
+        printf("Could not create window!\n");
+        fflush(stdout);
+        return nullptr;
+        exit(1);
+    }
+
+    ShowWindow(window, SW_SHOW);
+    SetForegroundWindow(window);
+    SetFocus(window);
+
+    return window;
+}
+#endif
