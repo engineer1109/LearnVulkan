b941d4d7d18ed8191bb0b3b3f80b978d858bc7d2
wjl <1292846099@qq.com>
2019-10-12 11:13:35 +0800

    update examples

--- include/basicengine/vulkan_basicengine.h
+++ include/basicengine/vulkan_basicengine.h
@@ -11,6 +11,7 @@ public:
     
     std::string getWindowTitle();
     virtual void initWindow();
+    virtual void setWindow(uint32_t window);
 
     void* getWindowPtr(){return m_window;}
     void setWindowFrameVisible(bool value){m_enableWindow=value;}
