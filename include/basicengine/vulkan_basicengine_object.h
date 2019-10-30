2215c98073b3f9fcdea9f57caeb746be0bf64d2d
engineer1109 <1292846099@qq.com>
2019-10-30 23:22:25 +0800

    update code

--- include/basicengine/vulkan_basicengine_object.h
+++ include/basicengine/vulkan_basicengine_object.h
@@ -1,4 +1,54 @@
 #ifndef VULKAN_BASICENGINE_OBJECT_H
 #define VULKAN_BASICENGINE_OBJECT_H
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <vulkan/vulkan.h>
+#include <glm/glm.hpp>
+#include "VulkanDevice.hpp"
 
+class VulkanBaseObject{
+public:
+    VulkanBaseObject()=default;
+    ~VulkanBaseObject()=default;
+public:
+    struct ObjectInfo{
+        vks::VulkanDevice* vulkanDevice=nullptr;
+        VkInstance instance=VK_NULL_HANDLE;
+        VkCommandPool cmdPool=VK_NULL_HANDLE;
+        VkPipelineLayout pipelineLayout=VK_NULL_HANDLE;
+        VkPipelineCache pipelineCache=VK_NULL_HANDLE;
+        VkRenderPass renderPass=VK_NULL_HANDLE;
+        VkQueue queue=VK_NULL_HANDLE;
+        uint32_t* screenWitdh=nullptr;
+        uint32_t* screenHeight=nullptr;
+    }m_objectInfo;
+public:
+    void setObjectInfo(ObjectInfo info){
+        this->m_vulkanDevice=info.vulkanDevice;
+        this->m_device=this->m_vulkanDevice->logicalDevice;
+        this->m_physicalDevice=this->m_vulkanDevice->physicalDevice;
+        this->m_instance=info.instance;
+        this->m_cmdPool=info.cmdPool;
+        this->m_pipelineLayout=info.pipelineLayout;
+        this->m_pipelineCache=info.pipelineCache;
+        this->m_pipelineLayout=info.pipelineLayout;
+        this->m_renderPass=info.renderPass;
+        this->m_queue=info.queue;
+        this->m_screenWidth=info.screenWitdh;
+        this->m_screenHeight=info.screenHeight;
+    }
+
+protected:
+    vks::VulkanDevice* m_vulkanDevice=nullptr;
+    VkDevice m_device=VK_NULL_HANDLE;
+    VkPhysicalDevice m_physicalDevice=VK_NULL_HANDLE;
+    VkInstance m_instance=VK_NULL_HANDLE;
+    VkCommandPool m_cmdPool=VK_NULL_HANDLE;
+    VkPipelineLayout m_pipelineLayout=VK_NULL_HANDLE;
+    VkPipelineCache m_pipelineCache=VK_NULL_HANDLE;
+    VkRenderPass m_renderPass=VK_NULL_HANDLE;
+    VkQueue m_queue=VK_NULL_HANDLE;
+    uint32_t* m_screenWidth=nullptr;
+    uint32_t* m_screenHeight=nullptr;
+};
 #endif // VULKAN_BASICENGINE_OBJECT_H
