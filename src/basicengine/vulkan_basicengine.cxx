8d385e42343ae2cb412c13d6c08d42f6e968ee56
engineer1109 <1292846099@qq.com>
2019-11-24 15:12:50 +0800

    update code

--- src/basicengine/vulkan_basicengine.cxx
+++ src/basicengine/vulkan_basicengine.cxx
@@ -1,3 +1,11 @@
+/*
+* LearnVulkan BasicEngine
+*
+* Copyright (C) by Jialiang Wang - https://github.com/engineer1109/LearnVulkan
+* 1292846099@qq.com
+*
+* This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+*/
 #include "vulkan_basicengine.h"
 VulkanBasicEngine::VulkanBasicEngine(bool enableValidation):VulkanExampleBase(enableValidation){
 
