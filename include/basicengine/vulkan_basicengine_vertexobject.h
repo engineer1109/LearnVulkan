15696c8b924ce4124ff3992b6166d1858c43f5f0
wjl <1292846099@qq.com>
2019-11-28 18:06:11 +0800

    new code

--- include/basicengine/vulkan_basicengine_vertexobject.h
+++ include/basicengine/vulkan_basicengine_vertexobject.h
@@ -1,4 +1,7 @@
 #ifndef VULKAN_BASICENGINE_VERTEXOBJECT_H
 #define VULKAN_BASICENGINE_VERTEXOBJECT_H
-
+#include "vulkan_basicengine_object.h"
+class VertexObject:public VulkanBaseObject{
+    VertexObject()=default;
+};
 #endif // VULKAN_BASICENGINE_VERTEXOBJECT_H
