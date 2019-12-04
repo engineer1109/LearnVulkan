e03d216668c10aa30942a230ed87c534a1014329
wjl <1292846099@qq.com>
2019-12-04 18:01:40 +0800

    add namespace

--- src/basicengine/vulkan_basicengine_vertexobject.cxx
+++ src/basicengine/vulkan_basicengine_vertexobject.cxx
@@ -1,4 +1,6 @@
 #include "vulkan_basicengine_vertexobject.h"
+namespace VulkanTemplate {
+
 VertexObject::~VertexObject(){
     if(m_pipeline){
         vkDestroyPipeline(m_device, m_pipeline, nullptr);
@@ -10,6 +12,7 @@ VertexObject::~VertexObject(){
 
 void VertexObject::create(){
     generateVertex();
+    prepareUniformBuffers();
 }
 
 void VertexObject::update(){
@@ -17,5 +20,22 @@ void VertexObject::update(){
 }
 
 void VertexObject::generateVertex(){
+
+}
+
+void VertexObject::prepareUniformBuffers(){
+    VK_CHECK_RESULT(m_vulkanDevice->createBuffer(
+        VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+        &m_uniformBuffers,
+        sizeof(m_uboVS),
+        &m_uboVS));
+
+    updateUniformBuffers();
+}
+
+void VertexObject::updateUniformBuffers(){
+
+}
 
 }
